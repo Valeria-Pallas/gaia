95173d602ab0ac88c1861a231040a8875472ab79
Valeria-Pallas <valerianpallas@gmail.com>
2023-03-03 11:48:55 +0100

    config aplication

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.assets.initialize_on_precompile = false
 module Gaia
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
